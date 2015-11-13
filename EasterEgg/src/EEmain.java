39b04f28c4807cfd092e8059b226221fc4536426
Trung Dang <tdang229@uw.edu>
2015-11-12 20:29:40 -0800

    Update EEmain.java

--- EasterEgg/src/EEmain.java
+++ EasterEgg/src/EEmain.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
 
 public class EEmain {
 
 	public static void main(String[] args) {
-		System.out.println("Trung");
+		System.out.println("Program is running. ")
+		Scanner stdIn = new Scanner(System.in);
+	
+		System.out.println("Type c c s to trigger an Easter Egg");
+		boolean bingo = false;
+		do {
+			char first = stdIn.nextChar();
+			char second = stdIn.nextChar();	
+			char third = stdIn.nextChar();
+			if (first == 'c' && second == 'c' && third = 's') {
+				bingo = true;
+			} else {
+				bingo = false;
+			}
+		} while (bingo = false);
+		display();
 
 	}
+	
+	private void display() {
+		System.out.println("The Crazy Code Surgeons - CCS");
+		
+		
+	}
+
 
 }
